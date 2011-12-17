048856ddce05c23642271dc654b0c6af84bc19af
Gihan Subhashana <gihanshp@gmail.com>
2011-12-17 19:03:18 +0530

    Added main plugin page.

---
+++ fbpagephotos.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: FB Page Photos 
+Plugin URI: https://github.com/gihanshp/fb-page-photos	
+Description: This plugin will fetch and display of Facebook page's uploaded photo albums and photos
+Version: 1.0
+Author: Gihan Subhashana
+Author URI: https://github.com/gihanshp
+License: GPL
+*/
+
+define('FB_API_URI', 'https://graph.facebook.com');
+define('FBPP_ALBUM_TBL', 'fbpp_album');
+define('FBPP_PHOTO_TBL', 'fbpp_photo');
