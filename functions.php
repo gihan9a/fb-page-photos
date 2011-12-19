44f7730c03f2434a084ff1f5ef9d3bb7b6ab0165
Gihan Subhashana <gihanshp@gmail.com>
2011-12-19 14:26:49 +0530

    Added curl option to get contents.

--- functions.php
+++ functions.php
@@ -2,13 +2,27 @@
 /**
  * Common functions are listed here
  */
+ 
+
+/**
+ * Curl implementation to get api data
+ */
+function curl_get_contents($url) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$output = curl_exec($ch);
+	curl_close($ch);
+	return $output;
+}
 
 /* 
  * Fetch albums from facbook api
  */
 function fbpp_fetch_albums($id){
 	$url = FB_API_URI.'/'.$id.'/albums';
-	$albums = file_get_contents($url);
+	$albums = curl_get_contents($url);
 	return json_decode($albums);
 }
 
@@ -17,7 +31,7 @@ function fbpp_fetch_albums($id){
  */
 function fbpp_fetch_photo($id){
 	$url = FB_API_URI.'/'.$id;
-	$photo = file_get_contents($url);
+	$photo = curl_get_contents($url);
 	return json_decode($photo);
 }
 
@@ -26,6 +40,23 @@ function fbpp_fetch_photo($id){
  */
 function fbpp_fetch_album_photos($id){
 	$url = FB_API_URI.'/'.$id.'/photos';
-	$photos = file_get_contents($url);
+	$photos = curl_get_contents($url);
 	return json_decode($photos);
+}
+
+
+/**
+ * Get all stored albums
+ */
+function fbpp_get_all_albums(){
+	global $wpdb;
+	return $wpdb->get_results("SELECT * FROM ".FBPP_ALBUM_TBL);
+}
+
+/**
+ * Get photos of 
+ */
+function fbpp_get_photo($fbid){
+	global $wpdb;
+	return $wpdb->get_row("SELECT * FROM ".FBPP_PHOTO_TBL." WHERE fbid='$fbid'");
 }
